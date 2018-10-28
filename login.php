443894da73d712433a587a2e83271bf6d327e299
EvNr <44534501+EvNr@users.noreply.github.com>
2018-10-28 11:45:45 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,15 +1,18 @@
-
-
-<html><head><script>
-function sendUserLoggedIn()
-{
-	var userDict = {};
-	userDict['name'] = 'EvNr';
-	userDict['uid'] = 'FollowMeonTwitterEvNr7';
-	userDict['email'] = 'EvNr@Modder.com';
-	userDict['token'] = '52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a';
-	document.location.href = 'accounts://login#'+escape(JSON.stringify(userDict));
-	alert("Cracked by "+userDict['name']);
-  alert("follow me on Twitter For more: @EvNr7");
-}
-</script></head><body onload="sendUserLoggedIn();">Follow me on Twitter: @EvNr7</body></html>
+<? php
+echo '';
+echo '';
+echo '<html><head><script>';
+echo 'function sendUserLoggedIn()';
+echo '{';
+echo 'var userDict = {};';
+echo 'userDict['name'] = 'EvNr';';
+echo 'userDict['uid'] = 'FollowMeonTwitterEvNr7';';
+echo 'userDict['email'] = 'EvNr@Modder.com';';
+echo 'userDict['token'] = '52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a';';
+echo 'document.location.href = 'accounts://login#'+escape(JSON.stringify(userDict));';
+echo 'alert("Cracked by "+userDict['name']);';
+echo 'alert("follow me on Twitter For more: @EvNr7");';
+echo '}';
+echo '</script></head><body onload="sendUserLoggedIn();">Follow me on Twitter: @EvNr7</body></html>';
+echo '';
+?>
