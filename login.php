34e2ec5e035fe3bf7d363cbe24982ea371f7dae7
EvNr <44534501+EvNr@users.noreply.github.com>
2018-10-28 12:11:46 +0300

    Delete login.php

--- login.php
+++
@@ -1,18 +0,0 @@
-<? php
-echo '';
-echo '';
-echo '<html><head><script>';
-echo 'function sendUserLoggedIn()';
-echo '{';
-echo 'var userDict = {};';
-echo 'userDict['name'] = 'EvNr';';
-echo 'userDict['uid'] = 'FollowMeonTwitterEvNr7';';
-echo 'userDict['email'] = 'EvNr@Modder.com';';
-echo 'userDict['token'] = '52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a52336104be246289fc8c4a76561d0b4fb825755a';';
-echo 'document.location.href = 'accounts://login#'+escape(JSON.stringify(userDict));';
-echo 'alert("Cracked by "+userDict['name']);';
-echo 'alert("follow me on Twitter For more: @EvNr7");';
-echo '}';
-echo '</script></head><body onload="sendUserLoggedIn();">Follow me on Twitter: @EvNr7</body></html>';
-echo '';
-?>
